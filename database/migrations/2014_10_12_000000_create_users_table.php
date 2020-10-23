ae19ad7e645d67baa0ebb507012557cabaa00f65
Riogunawan <riogunawan62587@gmail.com>
2020-10-23 10:45:17 +0700

    Update Kolom Database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->bigIncrements('ID_User');
+            $table->string('Username', 30);
+            $table->string('Password', 50);
+            $table->string('Nama', 50);
+            $table->date('Tanggal_Lahir');
+            $table->string('Alamat', 100);
+            $table->string('No_Telp', 15)->unique();
+            $table->string('Email', 30)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->enum('Jenis_Kelamin', ['laki - laki', 'permepuan']);
+            $table->integer('ID_Role');
+            $table->enum('Status_Akun', ['Terverifikasi', 'Belum Terverifikasi']);
             $table->rememberToken();
             $table->timestamps();
         });
